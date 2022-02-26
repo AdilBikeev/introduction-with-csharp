19da6145fc770c353e0cd68ee1661de06b07ec82
AdilBikeev <bikeev1999@mail.ru>
2022-02-26 14:58:27 +0300

    Add solution for Task-1

---
+++ introduction-with-csharp/1_Seminar/1_TwoNumbersMax/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число для 'a': ");
+var a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число для 'b': ");
+var b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"a = {a}; b = {b} -> max = {Math.Max(a, b)}");
