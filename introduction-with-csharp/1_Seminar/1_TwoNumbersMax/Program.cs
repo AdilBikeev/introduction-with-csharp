544c2a9494020c001e8f4b81026bcb85549bebf0
AdilBikeev <bikeev1999@mail.ru>
2022-02-26 15:28:32 +0300

    Finx wrong solution for task 1

--- introduction-with-csharp/1_Seminar/1_TwoNumbersMax/Program.cs
+++ introduction-with-csharp/1_Seminar/1_TwoNumbersMax/Program.cs
@@ -4,4 +4,4 @@ var a = int.Parse(Console.ReadLine());
 Console.Write("Введите число для 'b': ");
 var b = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"a = {a}; b = {b} -> max = {Math.Max(a, b)}");
+Console.WriteLine($"a = {a}; b = {b} -> max = {Math.Max(a, b)}, min = {Math.Min(a, b)}");
