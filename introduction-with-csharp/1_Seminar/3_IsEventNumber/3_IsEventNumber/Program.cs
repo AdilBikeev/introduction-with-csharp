ae383ed850035b317dcde0c6353c4037fb375d70
AdilBikeev <bikeev1999@mail.ru>
2022-02-26 15:46:16 +0300

    Add solution for task-3

---
+++ introduction-with-csharp/1_Seminar/3_IsEventNumber/3_IsEventNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Првоеряет является ли входное число четным.
+/// </summary>
+/// <param name="num">Входное целое число.</param>
+/// <return>True - Если число четное и False в ином случаи.</return>
+bool IsEvenNumber(int num) => num % 2 == 0;
+
+Console.Write("Введите число: ");
+var num = int.Parse(Console.ReadLine());
+var result = IsEvenNumber(num) ? "да" : "нет";
+
+Console.WriteLine($"{num} -> {result}");
