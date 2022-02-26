40e02a451eb9dc00dcb5f4d311710bb9ed55b612
AdilBikeev <bikeev1999@mail.ru>
2022-02-26 15:29:30 +0300

    Add solution for task 2

---
+++ introduction-with-csharp/1_Seminar/2_ThreeNumbersMax/2_ThreeNumbersMax/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для 'a': ");
+var a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число для 'b': ");
+var b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число для 'c': ");
+var c = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a}, {b}, {c} -> {Math.Max(Math.Max(a, b), c)}");
