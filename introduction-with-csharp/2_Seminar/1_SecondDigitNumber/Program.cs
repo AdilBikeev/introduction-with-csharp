76adf2d5f1e22ca03e139f16886263ec1ad0fad7
AdilBikeev <bikeev1999@mail.ru>
2022-02-27 17:00:43 +0300

    Add solution for Seminar 2 task 1

---
+++ introduction-with-csharp/2_Seminar/1_SecondDigitNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// Возвращает 2-ую цифру трехзначного числа.
+/// </summary>
+/// <param name="num">Трехзначное число.</param>
+int ParseSecondDigitNumber(int num) => (num / 10) // Убираем последнюю цифру у числа num -> останутся первые 2 цифры
+                                       % 10; // Берем последнюю цифру, т.к. она и будет 2-ой
+
+Console.Write("Введите 3-ех значное число: ");
+var num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num} -> {ParseSecondDigitNumber(num)}");
