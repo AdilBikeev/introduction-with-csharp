5ffb59b0a1a4814293e6faaf24737485978ddf80
AdilBikeev <bikeev1999@mail.ru>
2022-02-27 18:06:01 +0300

    add solution for task 4

---
+++ introduction-with-csharp/2_Seminar/4_IsWeekend/Program.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// Возвращает True если номер дня недели является выходным.
+/// </summary>
+bool IsWeekend(short numDayOfWeek) => 
+            numDayOfWeek == 6 || // суббота
+            numDayOfWeek == 7; // воскресенье
+
+Console.Write("Введите номер дня недели: ");
+var numDayOfWeek = short.Parse(Console.ReadLine());
+
+if (IsWeekend(numDayOfWeek))
+    Console.WriteLine($"{numDayOfWeek} -> да");
+else
+    Console.WriteLine($"{numDayOfWeek} -> нет");
