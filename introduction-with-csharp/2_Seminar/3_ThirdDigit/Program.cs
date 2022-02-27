191444e32e27ef9424474432f72dfadedb36386f
AdilBikeev <bikeev1999@mail.ru>
2022-02-27 17:50:15 +0300

    Add solution for task 3

---
+++ introduction-with-csharp/2_Seminar/3_ThirdDigit/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/// <summary>
+/// Пробует взять третью цифру из входного числа в виде строки.
+/// </summary>
+/// <param name="num">Входное число в виде строки.</param>
+/// <param name="result">Для хранения результата парсинга третьей цифры.</param>
+/// <return>Возващает True - если удалось распарсить.</return>
+bool TryParseThirdDigit(string num, out int result)
+{
+    result = default;
+    if (num.Length < 3)
+        return false;
+
+    result = int.Parse(
+        num.ElementAt(2) // Получаем 3-ю цифру числа
+           .ToString() // преобразуем в строку
+    ); // парсим строку в число
+
+    return true;
+}
+
+Console.Write("Введите число: ");
+var num = Console.ReadLine();
+
+if (TryParseThirdDigit(num, out int thirdDigit))
+    Console.WriteLine($"{num} -> {thirdDigit}");
+else
+    Console.WriteLine($"{num} -> третьей цифры нет");
