3d9557182e55110e30e21321794d8d81688240eb
AdilBikeev <bikeev1999@mail.ru>
2022-03-06 17:36:35 +0300

    Add solution for task 3

---
+++ introduction-with-csharp/3_Seminar/3_TableOfCube/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/// <summary>
+/// Возвращает список кубов чисел от 1 до N. 
+/// </summary>
+IReadOnlyList<int> GetCubesN(int N)
+{
+    var cubes = new List<int>(N) { 1 };
+    for (int i = 2; i <= N; i++)
+        cubes.Add(
+            (int)Math.Pow(i, 3)
+        );
+
+    return cubes;
+}
+
+Console.Write("Введите число N: ");
+var N = int.Parse(Console.ReadLine());
+var cubes = GetCubesN(N);
+
+Console.WriteLine($"{N} -> {string.Join(", ", cubes)}");
