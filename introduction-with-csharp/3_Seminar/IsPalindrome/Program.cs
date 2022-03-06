f85ee409f057fb879a232ef05f21b6705e3103dd
AdilBikeev <bikeev1999@mail.ru>
2022-03-06 16:20:11 +0300

    Add solution for 3_Seminar 1 task

---
+++ introduction-with-csharp/3_Seminar/IsPalindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Взодное число
+/// </summary>
+/// <param name="numStr">5-ти значное число в форме строки.</param>
+bool IsPalindrome(string numStr)
+{
+    for (int i = 0, j = numStr.Length - 1; 
+        i < j; 
+        i++, j--
+        )
+    {
+        if (numStr[i] != numStr[j])
+            return false;
+    }
+
+    return true;
+}
+
+
+Console.Write("Введите 5-ти значное число: ");
+var numStr = Console.ReadLine();
+
+var result = IsPalindrome(numStr) ? "да" : "нет";
+Console.WriteLine($"{numStr} -> {result}");
