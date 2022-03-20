e6e229bf77c53de9ddd86fb2a526bed295091bbc
AdilBikeev <bikeev1999@mail.ru>
2022-03-20 13:47:29 +0300

    Add Seminar 4 solution for task 1

---
+++ introduction-with-csharp/4_Seminar/1_Exponentiation/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/// <summary>
+/// Возводит число <paramref name="basisDegree"/> в степень <paramref name="indicatorDegree"/>
+/// </summary>
+/// <param name="basisDegree">Основание степени.</param>
+/// <param name="indicatorDegree">Показатель степени.</param>
+int Exponentiation(int basisDegree, int indicatorDegree)
+{
+    var res = 1;
+
+    while (indicatorDegree > 0)
+    {
+        res *= basisDegree;
+        indicatorDegree--;
+    }
+
+    return res;
+}
+
+Console.Write("Введите основание степени: ");
+var basisDegree = int.Parse(Console.ReadLine());
+
+Console.Write("Введите показатель степени: ");
+var indicatorDegree = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{basisDegree}, {indicatorDegree} -> " +
+    $"{Exponentiation(basisDegree, indicatorDegree)} " +
+    $"({basisDegree}^{indicatorDegree})");
