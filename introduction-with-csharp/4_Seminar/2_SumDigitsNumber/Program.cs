fe6d47a4adbe45e325674c49a13435accaa1de79
AdilBikeev <bikeev1999@mail.ru>
2022-03-20 13:53:34 +0300

    Add solution for task 2

---
+++ introduction-with-csharp/4_Seminar/2_SumDigitsNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/// <summary>
+/// Возвращает сумму цифр числа <paramref name="num" />
+/// </summary>
+int SumDigitsNumber(int num)
+{
+    var sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+var num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num} -> {SumDigitsNumber(num)}");
