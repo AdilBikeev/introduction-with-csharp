7556c67702a8da785da702d01903686c7bfdb742
AdilBikeev <bikeev1999@mail.ru>
2022-04-10 01:48:25 +0300

    Add solution for task 3

---
+++ introduction-with-csharp/9_Seminar/3_AckermanFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Вычисляет функцию Аккермана.
+/// </summary>
+int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+}
+
+
+Console.Write("Введите M: ");
+var m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+var n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"M={m}; N={n} -> A(m,n)={AckermanFunction(m, n)}");
