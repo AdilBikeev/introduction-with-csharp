e88b8af716ae63ea0d9a79fd81abea267d731ee4
AdilBikeev <bikeev1999@mail.ru>
2022-04-16 19:40:50 +0300

    Add solution for task 2

---
+++ introduction-with-csharp/11_Seminar/2_IsPrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Проверяет является ли входное число простым.
+/// </summary>
+/// <param name="num">Входное целое число.</param>
+bool IsPrimeNumber(int num) {
+    if (num < 2) return false; // принято считать 0 и 1 - НЕ простыми
+    if (num == 2) return true; // делится только на 2 и 1 - простое
+    if (num % 2 == 0) return false; // если число четное, то не простое
+    for (int i = 3; i * i <= num; i += 2) { // пробегаемся по НЕ четным числам, т.к. все четные - НЕ простые, при этом проверяем до квадрата числа, т.к. дальше числа не будет делиться
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+Console.Write("Введите число: ");
+var N = int.Parse(Console.ReadLine());
+var result = IsPrimeNumber(N) ? "Это простое число" : 
+                              "Это не простое число";
+
+Console.WriteLine($"N = {N} -> {result}");
